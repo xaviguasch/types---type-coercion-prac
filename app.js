8ae25044782afcef5cec45c6ec5d4ac2b20070b1
xaviguasch <xaviguasch@gmail.com>
2020-04-25 17:44:20 +0200

    added objects and comparisons prac

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ console.log([] + 1) // '' + 1 => '' + '1' => '1'
 console.log([1, 2] + 'hi') // '1,2' + 'hi' => '1,2hi'
 // 1. Step: toPrimitive() (internal method)
 // 2. Step: toString()
+
+console.log([1, 2] == 0) // NaN == 0 => false
+// console.log(Number([1, 2].toString()));
+console.log([] == 0) // '' == 0 => 0 == 0 => true
+console.log([] === 0)
