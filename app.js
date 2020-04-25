d563b6cb7b472e64401a3fb7b3071de905b1e938
xaviguasch <xaviguasch@gmail.com>
2020-04-25 17:45:26 +0200

    solved previous bug

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ console.log([1, 2] == 0) // NaN == 0 => false
 // console.log(Number([1, 2].toString()));
 console.log([] == 0) // '' == 0 => 0 == 0 => true
 console.log([] === 0)
+
+// console.log({}.toString());
+console.log({} + 1) // '[object Object]' + 1 => '[object Object]1'
+console.log({} + 'hi')
+console.log({} + []) // '[object Object]' + '' => '[object Object]'
+console.log({} == []) // '[object Object]' == ''
